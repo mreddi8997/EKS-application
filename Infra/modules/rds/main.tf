47771b1ded523db1cda5ba407e8b95bde3775794
Mredd <mreddi8997@gmail.com>
2026-09-22 19:56:36 -0230

    Add RDS subnet group, instance, and security group
    Added resources for RDS subnet group, instance, and security group with appropriate configurations.

--- Infra/modules/rds/main.tf
+++ Infra/modules/rds/main.tf
@@ -1,1 +1,48 @@
+resource "aws_db_subnet_group" "main" {
+  name       = "${var.vpc_name}-rds-subnet-group"
+  subnet_ids = var.rds_db_subnet_ids 
 
+  tags = {
+    Name = "${var.vpc_name}-rds-subnet-group"
+  }
+}
+
+resource "aws_db_instance" "main" {
+  allocated_storage    = var.rds_allocated_storage
+  storage_type         = var.rds_storage_type   
+  engine               = var.rds_engine
+  engine_version       = var.rds_engine_version
+  instance_class       = var.rds_instance_class
+  db_name              = var.rds_db_name
+  username             = var.rds_username
+  manage_master_user_password = true
+  parameter_group_name = var.rds_parameter_group_name
+  skip_final_snapshot  = var.rds_skip_final_snapshot
+  publicly_accessible  = false
+  vpc_security_group_ids = [aws_security_group.rds.id]
+  db_subnet_group_name   = aws_db_subnet_group.main.name
+
+  tags = {
+    Name = "${var.vpc_name}-rds-instance"
+  }
+}
+
+resource "aws_security_group" "rds" {
+  name        = "${var.vpc_name}-rds-sg"
+  description = "Security group for RDS instance"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432  
+    protocol    = "tcp"
+    security_groups = [var.rds_allowed_security_groups]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
