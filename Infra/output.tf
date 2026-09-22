93d2ae5504dc30dd769064f7d1376baa4a9c63bc
Mredd <mreddi8997@gmail.com>
2026-09-22 19:54:24 -0230

    Add EKS cluster outputs to output.tf

--- Infra/output.tf
+++ Infra/output.tf
@@ -1,1 +1,16 @@
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_arn" {
+  value = module.eks.cluster_arn
+}
+
+output "cluster_state" {
+  value = module.eks.cluster_state
+}
+
+output "node_iam_role_arn" {
+  value = module.eks.node_iam_role_arn
+}
 
