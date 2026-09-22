48a6dbb08525874ba886762103f1bfcfd8512221
Mredd <mreddi8997@gmail.com>
2026-09-22 19:55:32 -0230

    Add outputs for VPC and related resources

--- Infra/modules/vpc/output.tf
+++ Infra/modules/vpc/output.tf
@@ -1,1 +1,27 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
 
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "rds_subnet_ids" {
+  value = aws_subnet.rds[*].id
+}
+
+output "nat_gateway_id" {
+  value = aws_nat_gateway.main.id
+}
+
+output "external_ip" {
+  value = aws_eip.nat.public_ip
+}
+
+output "vpc_name" {
+  value = var.vpc_name
+}
