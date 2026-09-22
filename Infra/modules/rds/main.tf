58ad9d070694b8a4c531b24a3cf00061324fd426
Mredd <mreddi8997@gmail.com>
2026-09-22 20:24:47 -0230

    Fix security_groups assignment syntax in main.tf

--- Infra/modules/rds/main.tf
+++ Infra/modules/rds/main.tf
@@ -36,7 +36,7 @@ resource "aws_security_group" "rds" {
     from_port   = 5432
     to_port     = 5432  
     protocol    = "tcp"
-    security_groups = [var.rds_allowed_security_groups]
+    security_groups = var.rds_allowed_security_groups
   }
 
   egress {
