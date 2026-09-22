bc2fcb2910f9f0765ca2c95fd3ffb61765bb8cb0
Mredd <mreddi8997@gmail.com>
2026-09-22 19:55:55 -0230

    Add VPC and subnet variables to variables.tf

--- Infra/modules/vpc/variables.tf
+++ Infra/modules/vpc/variables.tf
@@ -1,1 +1,36 @@
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+  
+}
 
+variable "public_subnets" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "rds_subnets" {
+  description = "List of RDS subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.5.0/24", "10.0.6.0/24"]
+}
+
+variable "availability_zones" {
+  description = "List of availability zones for the subnets"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "vpc_name" {
+  description = "The name of the VPC"
+  type        = string
+  default     = "Project-VPC"
+}
