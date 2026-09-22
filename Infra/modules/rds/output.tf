96899f9fdb4ce1cbcb5b8e97072896e3507544b8
Mredd <mreddi8997@gmail.com>
2026-09-22 19:56:57 -0230

    Add outputs for RDS instance properties

--- Infra/modules/rds/output.tf
+++ Infra/modules/rds/output.tf
@@ -1,1 +1,12 @@
 
+output "name" {
+  value = aws_db_instance.main.db_name
+}
+
+output "db_master_secret_arn" {
+  value = aws_db_instance.main.master_user_secret[0].secret_arn
+}
+
+output "db_instance_endpoint" {
+  value = aws_db_instance.main.endpoint
+}
