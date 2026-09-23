b146f5bf3cdae1d295b6c6730a99e56eb3377445
Mredd <mreddi8997@gmail.com>
2026-09-23 17:47:27 -0230

    Update security group to remove ingress and egress rules
    Removed default ingress and egress rules from the security group.

--- Infra/modules/vpc/main.tf
+++ Infra/modules/vpc/main.tf
@@ -124,17 +124,6 @@ resource "aws_security_group" "vpc_sg" {
   description = "Main security group for VPC"
   vpc_id      = aws_vpc.main.id
 
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+ingress = []
+egress  = []
 }
