1a3ac482772d340e21f0e7f108e3239262eff45f
Mredd <mreddi8997@gmail.com>
2026-09-23 17:13:32 -0230

    Configure Terraform version and AWS provider
    Add Terraform required version and provider configuration.

--- Infra/modules/rds/main.tf
+++ Infra/modules/rds/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.10.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 resource "aws_db_subnet_group" "main" {
   name       = "${var.vpc_name}-rds-subnet-group"
   subnet_ids = var.rds_db_subnet_ids
