f5d510f38f3db57ae0bdcf35d3bb09737095e9d2
Mredd <mreddi8997@gmail.com>
2026-09-23 10:21:19 -0230

    Refactor VPC configuration in main.tf

--- Infra/modules/vpc/main.tf
+++ Infra/modules/vpc/main.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
+  cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
 
@@ -68,17 +68,16 @@ resource "aws_route_table_association" "public" {
 }
 
 resource "aws_nat_gateway" "main" {
-  count = length(var.public_subnets) > 0 ? 1 : 0
+  count         = length(var.public_subnets) > 0 ? 1 : 0
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public[0].id
-  
-depends_on = [aws_internet_gateway.main]
-  
-tags = {
+
+  depends_on = [aws_internet_gateway.main]
+
+  tags = {
     Name = "${var.vpc_name}-nat"
   }
 }
-
 
 resource "aws_eip" "nat" {
   domain = "vpc"
