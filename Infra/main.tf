d836b86c92b6302b56763bf952ce22805057816b
Mredd <mreddi8997@gmail.com>
2026-09-23 09:55:02 -0230

    Refactor RDS and EKS module configurations

--- Infra/main.tf
+++ Infra/main.tf
@@ -4,40 +4,40 @@ module "vpc" {
 
 module "rds" {
   source = "./modules/rds"
+
   vpc_name                    = module.vpc.vpc_name
-  rds_db_subnet_ids           = module.vpc.rds_subnet_ids
+  rds_db_subnet_ids            = module.vpc.rds_subnet_ids
   rds_allowed_security_groups = [module.eks.node_security_group_id]
-  rds_db_name                 = "customer_feedback_db"
-  rds_username                = "feedbackuser"
-  vpc_id                      = module.vpc.vpc_id
+  rds_db_name                  = "customer_feedback_db"
+  rds_username                 = "feedbackuser"
+  vpc_id                       = module.vpc.vpc_id
 
   depends_on = [module.vpc, module.eks]
 }
 
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "21.25.3"
-  name   = "Customer-feedback-application-cluster"
-  kubernetes_version = "1.35"
-  node_security_group_name = "Customer-feedback-application-node-sg"
-  security_group_name = "Customer-feedback-application-sg"
-  vpc_id = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnet_ids
 
+  name                    = "Customer-feedback-application-cluster"
+  kubernetes_version      = "1.35"
+  node_security_group_name = "Customer-feedback-application-node-sg"
+  security_group_name      = "Customer-feedback-application-sg"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnet_ids
 
   endpoint_public_access = true
 
-addons = {
-  vpc-cni = {
-    before_compute = true
+  addons = {
+    vpc-cni = {
+      before_compute = true
+    }
+    coredns    = {}
+    kube-proxy = {}
   }
-  coredns    = {}
-  kube-proxy = {}
-}
-  
 
-   eks_managed_node_groups = {
+  eks_managed_node_groups = {
     standard-node-group = {
       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
       ami_type       = "AL2023_x86_64_STANDARD"
