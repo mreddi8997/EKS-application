34344007087aba6a3aa572024c5184c319a986b1
Mredd <mreddi8997@gmail.com>
2026-09-23 17:43:13 -0230

    Fix typo in egress security_groups variable
    Corrected typo in security_groups variable name.

--- Infra/modules/rds/main.tf
+++ Infra/modules/rds/main.tf
@@ -53,10 +53,10 @@ resource "aws_security_group" "rds" {
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = var.rds_allowed_secuirty_groups 
   }
 }
 
