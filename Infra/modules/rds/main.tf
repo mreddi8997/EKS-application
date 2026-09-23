aa26d4e54e91161f3fe5985a622436c1f2472f57
Mredd <mreddi8997@gmail.com>
2026-09-23 10:18:48 -0230

    Format RDS module for consistency and readability

--- Infra/modules/rds/main.tf
+++ Infra/modules/rds/main.tf
@@ -1,6 +1,6 @@
 resource "aws_db_subnet_group" "main" {
   name       = "${var.vpc_name}-rds-subnet-group"
-  subnet_ids = var.rds_db_subnet_ids 
+  subnet_ids = var.rds_db_subnet_ids
 
   tags = {
     Name = "${var.vpc_name}-rds-subnet-group"
@@ -8,19 +8,19 @@ resource "aws_db_subnet_group" "main" {
 }
 
 resource "aws_db_instance" "main" {
-  allocated_storage    = var.rds_allocated_storage
-  storage_type         = var.rds_storage_type   
-  engine               = var.rds_engine
-  engine_version       = var.rds_engine_version
-  instance_class       = var.rds_instance_class
-  db_name              = var.rds_db_name
-  username             = var.rds_username
+  allocated_storage           = var.rds_allocated_storage
+  storage_type                = var.rds_storage_type
+  engine                      = var.rds_engine
+  engine_version              = var.rds_engine_version
+  instance_class              = var.rds_instance_class
+  db_name                     = var.rds_db_name
+  username                    = var.rds_username
   manage_master_user_password = true
-  parameter_group_name = var.rds_parameter_group_name
-  skip_final_snapshot  = var.rds_skip_final_snapshot
-  publicly_accessible  = false
-  vpc_security_group_ids = [aws_security_group.rds.id]
-  db_subnet_group_name   = aws_db_subnet_group.main.name
+  parameter_group_name        = var.rds_parameter_group_name
+  skip_final_snapshot         = var.rds_skip_final_snapshot
+  publicly_accessible         = false
+  vpc_security_group_ids      = [aws_security_group.rds.id]
+  db_subnet_group_name        = aws_db_subnet_group.main.name
 
   tags = {
     Name = "${var.vpc_name}-rds-instance"
@@ -33,9 +33,9 @@ resource "aws_security_group" "rds" {
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port   = 5432
-    to_port     = 5432  
-    protocol    = "tcp"
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
     security_groups = var.rds_allowed_security_groups
   }
 
