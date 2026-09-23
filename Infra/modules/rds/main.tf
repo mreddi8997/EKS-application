a208003eb186a5f19c89e200a372398b5d0b782a
Mredd <mreddi8997@gmail.com>
2026-09-23 18:05:56 -0230

    Enable storage encryption for RDS instance

--- Infra/modules/rds/main.tf
+++ Infra/modules/rds/main.tf
@@ -33,6 +33,7 @@ resource "aws_db_instance" "main" {
   vpc_security_group_ids      = [aws_security_group.rds.id]
   db_subnet_group_name        = aws_db_subnet_group.main.name
   kms_key_id                  = aws_kms_key.rds_key.arn
+  storage_encrypted           = true
 
 
   tags = {
